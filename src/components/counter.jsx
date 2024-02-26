60c215876f9dd7e373ad5af7b4629caca903ec3d
anantsri007 <53440328+anantsri007@users.noreply.github.com>
2024-02-26 20:06:00 +0530

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+function Counter() {
+   const counter = useSelector(state => state.counter);
+
+   return  (
+    <>
+       <div>{counter}</div>
+   
+    </>
+
+   )
+}
+
+
+export default Counter
